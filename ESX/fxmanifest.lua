9858b7a92018c153691a65ddaf65bc84216f75a0
Infinity Development <96385278+infinitydevelopment14@users.noreply.github.com>
2022-03-10 21:50:48 +0100

    Create fxmanifest.lua

---
+++ ESX/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+author 'Infinity Development'
+description 'Pickup Job'
+version '1.0'
+
+client_scripts {
+    'client/*.lua',
+    'config.lua'
+}
+
+server_script {
+    'server/*.lua',
+    'config.lua',
+}
